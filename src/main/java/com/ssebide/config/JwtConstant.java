d427d23e1154d6118a3d924e62924ac482950e20
ssebide <sebiddejohn136@gmail.com>
2025-02-10 21:58:26 +0300

    Configure Spring Security

---
+++ src/main/java/com/ssebide/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.ssebide.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="sbahbaysqsjnamsnqsuqsasmqisasmqksmqsaqssnxshcdwsxsxxz";
+    public static final String JWT_HEADER="Authorization";
+}
---
+++ src/main/java/com/ssebide/modal/User.java
@@ -0,0 +1,10 @@
+package com.ssebide.modal;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+}
